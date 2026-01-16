fafa7505dfaafa262d2e893e7e049934a6d98d64
XavierLopezM <xlopezm@espol.edu.ec>
2026-01-16 01:33:56 -0500

    Creando TipoResiduo
    Creando TipoResiduo dentro de Programa

---
+++ ED_P1_Grupo05/src/Programa/TipoResiduo.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Enum.java to edit this template
+ */
+package Programa;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author ED_P1_Grupo05
+ */
+public enum TipoResiduo implements Serializable{
+    ORGANICO, PLASTICO, VIDRIO, ELECTRONICO, METAL, PAPEL, OTRO
+}
